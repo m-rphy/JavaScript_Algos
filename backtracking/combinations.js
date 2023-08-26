db9a3a93944a24ff49689ec0f4d2a039282572de
William Murphy <w.williamjmurphy@gmail.com>
2023-08-25 18:00:51 -0600

    optimized combinations

--- backtracking/combinations.js
+++ backtracking/combinations.js
@@ -9,7 +9,9 @@ const combinations = (n, k) => {
     const combinations = [];
 
     const dfs = (index = 1, currCombination = []) => {
+        // Found a combination
         if (currCombination.length >= k) return combinations.push([...currCombination]);
+        // Gone out of bounds
         if (index > n) return;
 
         currCombination.push(index);
@@ -23,3 +25,28 @@ const combinations = (n, k) => {
 };
 
 console.log(combinations(3,2));
+
+
+// Optimized
+// N chose K -> C(n,k) -> N!/[(n-k)! *K!]
+// Time: O(k * C(n, k))
+const optimizedCombination = (n, k) => {
+    const combinations = [];
+
+    const dfs = (index = 1, currCombination = []) => {
+        // Found a combination
+        if (currCombination.length >= k) return combinations.push([...currCombination]);
+        if (index > n) return;
+
+        for (let j = index; j < n + 1; j++) {
+            currCombination.push(j);
+            dfs(j + 1, currCombination);
+            currCombination.pop();
+        };
+    };
+
+    dfs();
+    return combinations;
+}
+
+console.log(optimizedCombination(3,2));
