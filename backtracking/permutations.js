986266781526d1df86890965bd9bf308600f69ac
William Murphy <w.williamjmurphy@gmail.com>
2023-08-25 18:23:30 -0600

    added new permutation solutions

--- backtracking/permutations.js
+++ backtracking/permutations.js
@@ -6,7 +6,7 @@
 Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
  */
 
-// O(n! * n) time | O(n!) space
+// O(n! * n^2) time | O(n!) space
 const permute = (nums, idx = 0) => {
   if (nums.length === idx) return [[]];
   // recursively call function with next index
@@ -21,7 +21,7 @@ const permute = (nums, idx = 0) => {
   return output;
 };
 
-// O(n! * n) time | O(n!) space
+// O(n! * n^2) time | O(n!) space
 const permute2 = (nums) => {
   const output = [];
   const dfs = (idx) => {
@@ -38,3 +38,48 @@ const permute2 = (nums) => {
   dfs(0);
   return output;
 };
+
+// Time: O(n^2 * n!)
+function permutationsIterative(nums) {
+  let perms = [];
+  perms.push([]);
+
+  for (let n of nums) {
+      let nextPerms = [];
+      for (let p of perms) {
+          for (let i = 0; i < p.length + 1; i++) {
+              let pCopy = [...p];
+              pCopy.concat(p);
+              pCopy.splice(i, 0, n);
+              nextPerms.push(pCopy);
+          };
+      };
+      perms = nextPerms;
+  };
+  return perms;
+};
+
+console.log(permutationsIterative([1,2,3]));
+
+// Time: O(n^2 * n!)
+function permutationsRecursive(nums) {
+  return helper(0, nums);
+};
+
+function helper(i, nums) {
+  if (i == nums.length) {
+      return [[]];
+  }
+  let resPerms = [];
+  let perms = helper(i + 1, nums);
+  for (let p of perms) {
+      for (let j = 0; j < p.length + 1; j++) {
+          let pCopy = [...p];
+          pCopy.splice(j, 0, nums[i]);
+          resPerms.push(pCopy);
+      };
+  };
+  return resPerms; 
+};
+
+console.log(permutationsRecursive([1,2,3]));
